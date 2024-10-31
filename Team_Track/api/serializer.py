099f247070bca35c59e7cbb905f0ed864147da68
Maruf512 <marufsarkar512@gmail.com>
2024-10-31 21:34:11 +0600

    added serializer

---
+++ Team_Track/api/serializer.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from .models import User, Add_Employee, Add_Customer, Products, Sell_Invoice, Invoice_Challan, Pay_Employee, Cash_Memo
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class Add_EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Add_Employee
+        fields = '__all__'
+
+
+class Add_CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Add_Customer
+        fields = '__all__'
+
+
+class ProductsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Products
+        fields = '__all__'
+
+
+class Sell_InvoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sell_Invoice
+        fields = '__all__'
+
+
+class Invoice_ChallanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Invoice_Challan
+        fields = '__all__'
+
+
+class Pay_EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pay_Employee
+        fields = '__all__'
+
+
+class Cash_MemoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cash_Memo
+        fields = '__all__'
+
--- Team_Track/api/views.py
+++ Team_Track/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Add_Employee
+from .serializer import Add_EmployeeSerializer
 
 # Create your views here.
+class add_employee(generics.ListCreateAPIView):
+    queryset = Add_Employee.objects.all()
+    serializer_class = Add_EmployeeSerializer
